b1e879b5d651da8327f1045045328f7accc70214
songminseok <ske3056@gmail.com>
2023-04-20 00:05:44 +0900

    Using Images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+//퍼블릭으로 이미지를 옮기면 경로로도 사용가능!
+//두 가지 방법을 두루두루 사용하자!
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
