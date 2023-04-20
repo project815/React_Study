dd5b6f00fcc0ee35669dddd79afdc3ffb36b9667
songminseok <ske3056@gmail.com>
2023-04-20 13:35:49 +0900

    props

--- src/App.js
+++ src/App.js
@@ -4,32 +4,63 @@
 
 import './App.css';
 import {useState} from 'react'
+import Title from './components/title'
 
 
 function App() {
   // let name = "mario";
-  const [name, setName] = useState('mario');
+  // const [name, setName] = useState('mario');
 
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
-    {title: "bower's live stream", id:4},
+    {title: "bower's live stream", id:2},
     {title: "race on moo moo farm", id: 3},
   ])
 
-  const clickHandler = () =>{
+  const [showEvent, setShowEvent] = useState(true);
+  console.log(showEvent);
+
+  const clickHandler = (id) =>{
     // name = "luigi";
-    setName('luigi');
-    console.log(name);
+
+    // setName('luigi');
+    // setEvents(events.filter((event)=>{
+    //   return id != event.id;
+    // }))
+    setEvents((prevEvents)=>{
+      return prevEvents.filter((event)=>{
+        return id != event.id;
+      })
+    })
+    console.log("실행", id);
+
   }
 
+  const subtitle = "All the latest events in MarioLand";
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={clickHandler}>change name</button>
+      <Title title="Event is Your Area" subtitle={subtitle} />
+      <Title title="Another title" subtitle='another subtitle' />
+      {showEvent && (
+        <div>
+        <button onClick={()=>setShowEvent(false)}>hide button</button>
+      </div>
+      )}
+      {!showEvent && (
+        <div>
+        <button onClick={()=>setShowEvent(true)}>show button</button>
+      </div>
+      )
+
+      }
+            {/* <h1>My name is {name}</h1> */}
+      {/* <button onClick={clickHandler}>change name</button> */}
       {
-        events.map((event, index)=>(
+        showEvent && events.map((event, index)=>(
           <div key={event.id}>
               <h2>{"index : " + index}{", event.id : "+ event.id}{", event.title : " +event.title}</h2>
+              {/* <button onClick={(clickHandler(event.id))}>onClick</button> 바로실행 되어버림, 클릭해도 실행안됨 */}
+              <button onClick={()=>(clickHandler(event.id))}>Delete event</button>
           </div>
         ))
       }
