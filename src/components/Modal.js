ed09c78326de1b5debd232bf0b4f5187f6d51f7f
songminseok <ske3056@gmail.com>
2023-04-20 16:42:42 +0900

    Portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,13 @@
+import ReactDOM from 'react-dom'
 import './Modal.css'
 
 export default function Modal({children}) {
-  return (
+  return ReactDOM.createPortal((
     <div className="modal-backdrop">
       <div className='modal'>
        {/* {props.children} */}
        {children}
       </div>
     </div>
-  )
+  ), document.body)
 }
