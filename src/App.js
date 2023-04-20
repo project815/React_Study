8ffbd11d668f65f59b961aef098131b4f3d3f425
songminseok <ske3056@gmail.com>
2023-04-20 15:02:17 +0900

    Children Prop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@
 import './App.css';
 import React, {useState} from 'react'
 import Title from './components/title'
-
+import Modal from './components/Modal'
 
 function App() {
   // let name = "mario";
@@ -40,7 +40,18 @@ function App() {
   return (
     <div className="App">
       <Title title="Event is Your Area" subtitle={subtitle} />
+      <br/>
       <Title title="Another title" subtitle='another subtitle' />
+      {/* <Modal>
+        <h2>10% 0ff Coupon Code!!</h2>
+        <p>Use the code INAK10 at the checkout!</p>
+      </Modal> */}
+      <Modal>
+        <h2>Term and Conditions</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <a href='https://www.naver.com'>find more out...</a>
+      </Modal>
+      
       {showEvent && (
         <div>
         <button onClick={()=>setShowEvent(false)}>hide button</button>
